60f7b2072ebb4646c1e914ac72a37bf47df2e6ed
GONNIM <suauncle@gmail.com>
2025-07-02 20:07:00 +0900

    Modified strategy_v2.py

--- core/strategy_v2.py
+++ core/strategy_v2.py
@@ -20,10 +20,9 @@ class MACDStrategy(Strategy):
     stop_loss = 0.01  # 1%
     macd_threshold = 0.0
     min_holding_period = 1
-    macd_crossover_threshold = 0.0
 
     def init(self):
-        logger.info("전략 초기화 시작")
+        logger.info("전략 초기화")
         close = self.data.Close
         self.macd_line = self.I(
             self._calculate_macd, close, self.fast_period, self.slow_period
@@ -37,9 +36,6 @@ class MACDStrategy(Strategy):
         self.last_signal_bar = None
 
         MACDStrategy.signal_events = []
-        logger.info(
-            f"init: MACDStrategy.signal_events id={id(MACDStrategy.signal_events)}"
-        )
 
     def _calculate_macd(self, series, fast, slow):
         series = pd.Series(series)
@@ -52,6 +48,18 @@ class MACDStrategy(Strategy):
         self.entry_price = None
         self.entry_bar = None
 
+    def _is_gold_cross(self):
+        return (
+            self.macd_line[-2] <= self.signal_line[-2]
+            and self.macd_line[-1] > self.signal_line[-1]
+        )
+
+    def _is_dead_cross(self):
+        return (
+            self.macd_line[-2] >= self.signal_line[-2]
+            and self.macd_line[-1] < self.signal_line[-1]
+        )
+
     def next(self):
         current_bar = len(self.data) - 1
         current_price = self.data.Close[-1]
@@ -63,14 +71,8 @@ class MACDStrategy(Strategy):
 
         cross = (
             "Gold"
-            if self.macd_line[-2] <= self.signal_line[-2]
-            and self.macd_line[-1] > self.signal_line[-1]
-            else (
-                "Dead"
-                if self.macd_line[-2] >= self.signal_line[-2]
-                and self.macd_line[-1] < self.signal_line[-1]
-                else "Neutral"
-            )
+            if self._is_gold_cross()
+            else "Dead" if self._is_dead_cross() else "Neutral"
         )
 
         # 로그 기록
@@ -79,52 +81,35 @@ class MACDStrategy(Strategy):
         )
 
         if self.position:
-            bars_since_entry = current_bar - self.entry_bar
+            bars_held = current_bar - self.entry_bar
+
             tp_price = self.entry_price * (1 + self.take_profit + 2 * MIN_FEE_RATIO)
             sl_price = self.entry_price * (1 - self.stop_loss - 2 * MIN_FEE_RATIO)
 
-            if current_price >= tp_price:
-                self.position.close()
-                MACDStrategy.signal_events.append(
-                    (current_bar, "SELL", "TP", macd_val, signal_val)
-                )
-                self._reset_entry()
-                self.last_signal_bar = current_bar
-                return
-
-            if current_price <= sl_price:
-                self.position.close()
-                MACDStrategy.signal_events.append(
-                    (current_bar, "SELL", "SL", macd_val, signal_val)
-                )
-                self._reset_entry()
-                self.last_signal_bar = current_bar
-                return
-
-            if bars_since_entry < self.min_holding_period:
-                return
-
-            if (
-                macd_val - signal_val < -self.macd_crossover_threshold
-                and self.macd_line[-2] >= self.signal_line[-2]
-                and macd_val >= self.macd_threshold
-            ):
-                self.position.close()
-                MACDStrategy.signal_events.append(
-                    (current_bar, "SELL", cross, macd_val, signal_val)
-                )
-                self._reset_entry()
-                self.last_signal_bar = current_bar
+            if bars_held >= self.min_holding_period:
+                if self._is_dead_cross():
+                    if current_price >= tp_price:
+                        self.position.close()
+                        MACDStrategy.signal_events.append(
+                            (current_bar, "SELL", "TP+DeadCross", macd_val, signal_val)
+                        )
+                        self._reset_entry()
+                        self.last_signal_bar = current_bar
+                        return
+                    elif current_price <= sl_price:
+                        self.position.close()
+                        MACDStrategy.signal_events.append(
+                            (current_bar, "SELL", "SL+DeadCross", macd_val, signal_val)
+                        )
+                        self._reset_entry()
+                        self.last_signal_bar = current_bar
+                        return
 
         else:
-            if (
-                macd_val - signal_val > self.macd_crossover_threshold
-                and self.macd_line[-2] <= self.signal_line[-2]
-                and macd_val >= self.macd_threshold
-            ):
+            if self._is_gold_cross() and macd_val >= self.macd_threshold:
                 self.buy()
                 MACDStrategy.signal_events.append(
-                    (current_bar, "BUY", cross, macd_val, signal_val)
+                    (current_bar, "BUY", "GoldCross", macd_val, signal_val)
                 )
                 self.entry_price = current_price
                 self.entry_bar = current_bar
