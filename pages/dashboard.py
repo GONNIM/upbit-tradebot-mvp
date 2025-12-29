148746cf172924b4aedd1f4e60f95aaa2246ceaa
GONNIM <suauncle@gmail.com>
2025-12-29 22:02:10 +0900

    feature: data_feed jitter 수정

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -210,7 +210,7 @@ if not engine_status:
 
 
 # ✅ 상단 정보
-st.markdown(f"### 📊 Dashboard ({mode}) : `{user_id}`님 --- v1.2025.12.29.2153")
+st.markdown(f"### 📊 Dashboard ({mode}) : `{user_id}`님 --- v1.2025.12.29.2201")
 st.markdown(f"🕒 현재 시각: {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
 col1, col2 = st.columns([4, 1])
